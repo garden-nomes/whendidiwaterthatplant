6176a0e24b28774f02ad439f4fa8bdc7499e5116
noaner <noahbweiner@gmail.com>
2018-12-05 18:42:53 -0800

    Serve client from express

--- server/app.ts
+++ server/app.ts
@@ -6,8 +6,8 @@ const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
-// const buildPath = path.join(__dirname, "..", "dist");
-// app.use(express.static(buildPath));
+const buildPath = path.join(__dirname, "..", "..", "dist");
+app.use(express.static(buildPath));
 
 app.use(morgan("combined"));
 
